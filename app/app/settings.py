400797a185f1a3c1b57d73799837668993d3be73
sachint97 <cssachinshoba@gmail.com>
2023-05-12 01:27:46 +0530

    reset config

--- app/app/settings.py
+++ app/app/settings.py
@@ -83,7 +83,6 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': os.environ.get('DB_PORT')
     }
 }
 
