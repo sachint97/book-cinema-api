a903d926f9615e69b44de03d0104cfbe858febad
sachint97 <cssachinshoba@gmail.com>
2023-05-12 00:31:47 +0530

    created custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,51 @@
-from django.db import models # noqa
+"""Database models."""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **kwargs):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)  # using=self._dbis for multiple database
+        return user
+
+    def create_superuser(self, email, password=None, **kwargs):
+        """Create and return new super user"""
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    deleted_at = models.DateTimeField(
+        auto_now_add=False,
+        auto_now=False,
+        blank=True, null=True
+        )
+
+    objects = UserManager()  # Assign usermanager to User model
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.name
